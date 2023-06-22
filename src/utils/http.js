d63e913aeca713e981617f76b4b62f0715f5877c
pei zhang <1015772292@qq.com>
2023-06-22 18:33:44 +0800

    axios基础封装

---
+++ src/utils/http.js
@@ -0,0 +1,23 @@
+//axios基础的封装
+import axios from "axios";
+const httpInstance = axios.create({
+	baseURL: "http://pcapi-xiaotuxian-front-devtest.iteima.net",
+	timeout: 5000,
+});
+
+//拦截器配置
+httpInstance.interceptors.request.use(
+	config => {
+		return config;
+	},
+	e => Promise.reject(e)
+);
+
+httpInstance.interceptors.response.use(
+	res => res.data,
+	e => {
+		return Promise.reject(e);
+	}
+);
+
+export default httpInstance;
