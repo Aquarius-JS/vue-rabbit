7a1fedbbec1ba91c69aeef0366858de8a6b4f544
pei zhang <1015772292@qq.com>
2023-06-22 19:45:24 +0800

    layout静态结构搭建

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,9 @@
 
 <template>
 	<router-view></router-view>
-	<div class="test">test scss</div>
 </template>
 
 <style scoped lang="scss">
-	.test {
-		color: $priceColor;
-	}
 	header {
 		line-height: 1.5;
 		max-height: 100vh;
--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
+//引入初始化的样式文件
+import "@/styles/common.scss";
 
 //测试接口函数
-import { getCategory } from '@/apis/testAPI';
-getCategory().then(res=>{
-    console.log(res);
-})
+import { getCategory } from "@/apis/testAPI";
+getCategory().then(res => {
+	console.log(res);
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
