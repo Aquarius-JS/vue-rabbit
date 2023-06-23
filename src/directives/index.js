f4f7205ac730d72682d0afdbb03d83561e7f2766
pei zhang <1015772292@qq.com>
2023-06-23 12:11:22 +0800

    v-img-lazy插件与优化

---
+++ src/directives/index.js
@@ -0,0 +1,20 @@
+//定义懒加载插件
+import { useIntersectionObserver } from "@vueuse/core";
+
+export const lazyPlugin = {
+	install(app) {
+		app.directive("img-lazy", {
+			mounted(el, binding) {
+				//el指令绑定的元素
+				//binding： binding.value 指令后面绑定的绑定的表达式的值
+				console.log(binding);
+				const {stop} = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+					if (isIntersecting) {
+						el.src = binding.value;
+                        stop();
+					}
+				});
+			},
+		});
+	},
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { useIntersectionObserver } from "@vueuse/core";
+import { lazyPlugin } from "./directives";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,21 +10,8 @@ import "@/styles/common.scss";
 
 const app = createApp(App);
 
+app.use(lazyPlugin);
 app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
-
-app.directive("img-lazy", {
-	mounted(el, binding) {
-		//el指令绑定的元素
-		//binding： binding.value 指令后面绑定的绑定的表达式的值
-		console.log(binding);
-		console.log(111);
-		useIntersectionObserver(el, ([{ isIntersecting }]) => {
-			if (isIntersecting) {
-				el.src = binding.value;
-			}
-		});
-	},
-});
