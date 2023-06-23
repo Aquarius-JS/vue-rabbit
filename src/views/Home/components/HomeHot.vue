60555355c0b6efbb235ec78cd653d55df1e40a73
pei zhang <1015772292@qq.com>
2023-06-23 10:57:52 +0800

    home 面版组件封装

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -1,5 +1,6 @@
+<script setup>
+	import HomePanel from "./HomePanel.vue";
+</script>
 <template>
-    <div>
-        我是人气推荐
-    </div>
+	<HomePanel title="人气推荐" sub-title="人气推荐 好多商品"> </HomePanel>
 </template>
--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,5 +1,6 @@
+<script setup>
+	import HomePanel from "./HomePanel.vue";
+</script>
 <template>
-    <div>
-        我是新鲜好物
-    </div>
+	<HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱"> </HomePanel>
 </template>
