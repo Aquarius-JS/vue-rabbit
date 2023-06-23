86bbe182af79dc88c1f6cfa14bfece27675fcad8
pei zhang <1015772292@qq.com>
2023-06-23 22:41:07 +0800

    放大镜效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 	import { ref } from "vue";
+	import { useMouseInElement } from "@vueuse/core";
 	// 图片列表
 	const imageList = [
 		"https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -13,6 +14,32 @@
 	function enterhandler(i) {
 		activeIndex.value = i;
 	}
+
+	//2.获取鼠标相对位置
+	const target = ref(null);
+	const { elementX, elementY, isOutside } = useMouseInElement(target);
+
+	//3.控制滑块跟随鼠标移动(监听elementX/Y变化,一旦变化,重新设置left/top)
+	const left = ref(0);
+	const top = ref(0);
+	watch([elementX, elementY], () => {
+		if (elementX.value > 100 && elementX < 300) {
+			left.value = elementX.value - 100;
+		}
+		if (elementY.value > 100 && elementY < 300) {
+			top.value = elementY.value - 100;
+		}
+		if (elementX.value > 300) {
+			left.value = 200;
+		} else if (elementX.value < 100) {
+			left.value = 0;
+		}
+		if (elementY.value > 300) {
+			top.value = 200;
+		} else if (elementY.value < 100) {
+			top.value = 0;
+		}
+	});
 </script>
 
 <template>
@@ -21,7 +48,7 @@
 		<div class="middle" ref="target">
 			<img :src="imageList[activeIndex]" alt="" />
 			<!-- 蒙层小滑块 -->
-			<div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+			<div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
 		</div>
 		<!-- 小图列表 -->
 		<ul class="small">
@@ -39,7 +66,7 @@
 			class="large"
 			:style="[
 				{
-					backgroundImage: `url(${imageList[0]})`,
+					backgroundImage: `url(${imageList[activeIndex]})`,
 					backgroundPositionX: `0px`,
 					backgroundPositionY: `0px`,
 				},
