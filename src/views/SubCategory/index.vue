5846365273bee1bfd3b1ec04d96619e228be4f0d
pei zhang <1015772292@qq.com>
2023-06-23 19:47:13 +0800

    二级分类面包屑导航实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+	import { getCategoryFilterAPI } from "@/apis/category";
+	import { useRoute } from "vue-router";
+	const route = useRoute();
+	const categoryData = ref({});
+	const getCategoryData = async () => {
+		let res = getCategoryFilterAPI(route.params.id);
+		categoryData.value = res.result;
+	};
+	onMounted(() => {
+		getCategoryData();
+	});
+</script>
 
 <template>
 	<div class="container">
@@ -6,8 +18,8 @@
 		<div class="bread-container">
 			<el-breadcrumb separator=">">
 				<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-				<el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
-				<el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+				<el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }} </el-breadcrumb-item>
+				<el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
 			</el-breadcrumb>
 		</div>
 		<div class="sub-container">
