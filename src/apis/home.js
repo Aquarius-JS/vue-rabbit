1c1a535b9c412a90294c73f7f3c23c459c5f37aa
pei zhang <1015772292@qq.com>
2023-06-23 17:03:32 +0800

    home 轮播图

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,11 @@
 import httpInstance from "@/utils/http";
-export function getBannerAPI() {
+export function getBannerAPI(params = {}) {
+	const { distributionSite = "1" } = params;
 	return httpInstance({
 		url: "/home/banner",
+		params: {
+			distributionSite,
+		},
 	});
 }
 /**
