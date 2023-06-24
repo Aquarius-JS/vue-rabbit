c27196ff3e329616f57cad0663948fa55a40732e
pei zhang <1015772292@qq.com>
2023-06-24 21:18:09 +0800

    接口购物车-列表购物车-合并本地购物车到服务器

--- src/apis/cart.js
+++ src/apis/cart.js
@@ -11,18 +11,27 @@ export const insertCartAPI = ({ skuId, count }) => {
 	});
 };
 //获取最新的购物车列表
-export const findNewCartListAPI = ()=>{
-    return httpInstance({
-        url:'/member/cart'
-    })
-}
+export const findNewCartListAPI = () => {
+	return httpInstance({
+		url: "/member/cart",
+	});
+};
 // 删除购物车
-export const delCartAPI = (ids) => {
-    return httpInstance({
-      url: '/member/cart',
-      method: 'DELETE',
-      data: {
-        ids
-      }
-    })
-  }
+export const delCartAPI = ids => {
+	return httpInstance({
+		url: "/member/cart",
+		method: "DELETE",
+		data: {
+			ids,
+		},
+	});
+};
+
+//合并购物车
+export const mergeCartAPI = data => {
+	return httpInstance({
+		url: "/member/cart/merge",
+		method: "POST",
+		data,
+	});
+};
--- src/stores/user.js
+++ src/stores/user.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { loginAPI } from "@/apis/user";
 import { useCartStore } from "@/stores/cartStore";
-
+import { mergeCartAPI } from "@/apis/cart";
 export const useUserStore = defineStore(
 	"user",
 	() => {
@@ -14,6 +14,16 @@ export const useUserStore = defineStore(
 		const getUserInfo = async ({ account, password }) => {
 			const res = await loginAPI({ account, password });
 			userInfo.value = res.result;
+			await mergeCartAPI(
+				cartStore.cartList.map(item => {
+					return {
+						skuId: item.skuId,
+						selected: item.selected,
+						count: item.count,
+					};
+				})
+			);
+			cartStore.updateNewList();
 		};
 		// 退出时清除用户信息
 		const clearUserInfo = () => {
