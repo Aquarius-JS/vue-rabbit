c3bdb3a892787220d0e2825a8742bded6d40e9ca
pei zhang <1015772292@qq.com>
2023-06-24 16:25:34 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { useUserStore } from "@/stores/user";
+import { useRouter } from "vue-router";
+const router = useRouter();
 const httpInstance = axios.create({
 	baseURL: "http://pcapi-xiaotuxian-front-devtest.iteima.net",
 	timeout: 5000,
@@ -29,6 +31,12 @@ httpInstance.interceptors.response.use(
 			type: "warning",
 			message: e.response.data.message,
 		});
+		//401token失效处理
+		if (e.response.status === 401) {
+			const userStore = useUserStore();
+			userStore.clearUserInfo();
+			router.push("/login");
+		}
 		return Promise.reject(e);
 	}
 );
